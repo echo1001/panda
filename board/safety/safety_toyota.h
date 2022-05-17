e277646ad1cb601d9a281fa3b75ca1997d8e0728
echo1001 <echo1001@ungclan.com>
2022-05-17 14:43:59 -0500

    Allow LTA Commands

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -196,21 +196,6 @@ static int toyota_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
       }
     }
 
-    // LTA steering check
-    // only sent to prevent dash errors, no actuation is accepted
-    if (addr == 0x191) {
-      // check the STEER_REQUEST, STEER_REQUEST_2, and STEER_ANGLE_CMD signals
-      bool lta_request = (GET_BYTE(to_send, 0) & 1U) != 0U;
-      bool lta_request2 = ((GET_BYTE(to_send, 3) >> 1) & 1U) != 0U;
-      int lta_angle = (GET_BYTE(to_send, 1) << 8) | GET_BYTE(to_send, 2);
-      lta_angle = to_signed(lta_angle, 16);
-
-      // block LTA msgs with actuation requests
-      if (lta_request || lta_request2 || (lta_angle != 0)) {
-        tx = 0;
-      }
-    }
-
     // STEER: safety check on bytes 2-3
     if (addr == 0x2E4) {
       int desired_torque = (GET_BYTE(to_send, 1) << 8) | GET_BYTE(to_send, 2);
